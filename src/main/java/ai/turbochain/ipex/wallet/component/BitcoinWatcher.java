c1b4866a168578352391db3085199b585450c17d
zhangmingcheng <zhangmingcheng@pansoft.com>
2019-07-25 09:20:47 +0800

    比特币测试网

--- src/main/java/ai/turbochain/ipex/wallet/component/BitcoinWatcher.java
+++ src/main/java/ai/turbochain/ipex/wallet/component/BitcoinWatcher.java
@@ -1,73 +1,74 @@
 package ai.turbochain.ipex.wallet.component;
 
-import com.spark.blockchain.rpcclient.Bitcoin;
-import com.spark.blockchain.rpcclient.BitcoinRPCClient;
-import ai.turbochain.ipex.wallet.entity.Deposit;
-import ai.turbochain.ipex.wallet.service.AccountService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.spark.blockchain.rpcclient.Bitcoin;
+import com.spark.blockchain.rpcclient.BitcoinRPCClient;
+
+import ai.turbochain.ipex.wallet.entity.Deposit;
+import ai.turbochain.ipex.wallet.service.AccountService;
+
 @Component
-public class BitcoinWatcher extends Watcher{
-    @Autowired
-    private BitcoinRPCClient rpcClient;
-    @Autowired
-    private AccountService accountService;
+public class BitcoinWatcher extends Watcher {
+	@Autowired
+	private BitcoinRPCClient rpcClient;
+	@Autowired
+	private AccountService accountService;
 
-    @Override
-    public List<Deposit> replayBlock(Long startBlockNumber, Long endBlockNumber) {
-        List<Deposit> deposits = new ArrayList<Deposit>();
-        try {
-            for (Long blockHeight = startBlockNumber; blockHeight <= endBlockNumber; blockHeight++) {
-                String blockHash = rpcClient.getBlockHash(blockHeight.intValue());
-                //获取区块
-                Bitcoin.Block block =  rpcClient.getBlock(blockHash);
-                List<String> txids = block.tx();
-                //遍历区块中的交易
-                for(String txid:txids){
-                    Bitcoin.RawTransaction transaction =  rpcClient.getRawTransaction(txid);
-                    List<Bitcoin.RawTransaction.Out> outs = transaction.vOut();
-                    if(outs != null) {
-                        for (Bitcoin.RawTransaction.Out out : outs) {
-                            if (out.scriptPubKey() != null) {
-                                String address = out.scriptPubKey().addresses().get(0);
-                                BigDecimal amount = new BigDecimal(out.value());
-                                if (accountService.isAddressExist(address)) {
-                                    Deposit deposit = new Deposit();
-                                    deposit.setTxid(transaction.txId());
-                                    deposit.setBlockHeight((long) block.height());
-                                    deposit.setBlockHash(transaction.blockHash());
-                                    deposit.setAmount(amount);
-                                    deposit.setAddress(address);
-                                    deposit.setTime(transaction.time());
-                                    deposits.add(deposit);
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        catch (Exception e){
-            e.printStackTrace();
-        }
-        return deposits;
-    }
+	@Override
+	public List<Deposit> replayBlock(Long startBlockNumber, Long endBlockNumber) {
+		List<Deposit> deposits = new ArrayList<Deposit>();
+		try {
+			for (Long blockHeight = startBlockNumber; blockHeight <= endBlockNumber; blockHeight++) {
+				String blockHash = rpcClient.getBlockHash(blockHeight.intValue());
+				// 获取区块
+				Bitcoin.Block block = rpcClient.getBlock(blockHash);
+				List<String> txids = block.tx();
+				// 遍历区块中的交易
+				for (String txid : txids) {
+					Bitcoin.RawTransaction transaction = rpcClient.getRawTransaction(txid);
+					List<Bitcoin.RawTransaction.Out> outs = transaction.vOut();
+					if (outs != null) {
+						for (Bitcoin.RawTransaction.Out out : outs) {
+							if (out.scriptPubKey() != null) {
+								if (out.scriptPubKey().addresses() == null) {
+									continue;
+								}
+								String address = out.scriptPubKey().addresses().get(0);
+								BigDecimal amount = new BigDecimal(out.value()).setScale(8, BigDecimal.ROUND_DOWN);
+								if (accountService.isAddressExist(address)) {                              
+									Deposit deposit = new Deposit();
+									deposit.setTxid(transaction.txId());
+									deposit.setBlockHeight((long) block.height());
+									deposit.setBlockHash(transaction.blockHash());
+									deposit.setAmount(amount);
+									deposit.setAddress(address);
+									deposit.setTime(transaction.time());								
+									deposits.add(deposit);
+								}
+							}
+						}
+					}
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return deposits;
+	}
 
-
-
-    @Override
-    public Long getNetworkBlockHeight() {
-        try {
-            return Long.valueOf(rpcClient.getBlockCount());
-        }
-        catch (Exception e){
-            e.printStackTrace();
-            return 0L;
-        }
-    }
+	@Override
+	public Long getNetworkBlockHeight() {
+		try {
+			return Long.valueOf(rpcClient.getBlockCount());
+		} catch (Exception e) {
+			e.printStackTrace();
+			return 0L;
+		}
+	}
 }
